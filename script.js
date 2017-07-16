5b4f5d70775687a7ebdf058eca949fff4bafd634
Mike Marlow <michael.s.marlow@gmail.com>
2017-07-16 14:42:36 -0600

    Built basic prepend function to replace placeholder

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+function prependIdea() {
+  var $title = $("#input-title").val();
+  var $content = $("#input-content").val();
+  var $inject =
+        `<article class="card">
+        <h2 id="idea-title">${title}</h2>
+        <button id="delete-btn"></button>
+        <p id="idea-content">${content}</p>
+        <div id="btn-quality-wrapper">
+          <button id="upvote"></button>
+          <button id="downvote"></button>
+          <p id="idea-quality">quality: chron</p>
+        </div>
+        <hr>
+      </article>`;
+
+  $(".idea-stage").prepend(inject);
+}
